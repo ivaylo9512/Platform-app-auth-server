8fe6ed75c04365d45e4279bde9d3564f2b99f7fc
ivaylo <ivayloaleksandrov@yahoo.com>
2021-06-29 13:55:46 +0300

    apollo + express added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-import { MikroORM } from '@mikro-orm/core'
+import { MikroORM } from '@mikro-orm/core';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import UserResolver from './resolvers/User';
 
 const main = async () => {
-    const orm = MikroORM.init(mikroConfig)
+    const orm = await MikroORM.init(mikroConfig)
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [UserResolver],
+            validate: false
+        }),
+        context: { em: orm.em } 
+    })
+
+    apolloServer.applyMiddleware({ app });
+
+    const PORT = process.env.PORT || 8056;
+    app.listen(PORT, () => {
+        console.log(`\n🚀!! server started on http://localhost:${PORT} !!`)
+    })
 }
 main().catch(err => console.log(err));
