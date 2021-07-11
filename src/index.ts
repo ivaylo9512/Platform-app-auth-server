82d30fec41f255c296c45691121d803402f7e898
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-11 22:47:27 +0300

    setting routers + bodyParsere + error handler + RequestContext

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { MikroORM } from '@mikro-orm/core';
+import { MikroORM, RequestContext } from '@mikro-orm/core';
 import mikroConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
@@ -6,10 +6,15 @@ import { buildSchema } from 'type-graphql';
 import UserResolver from './resolvers/User';
 import cors from 'cors';
 import Redis from 'ioredis';
+import UserServiceImpl from './services/user-service-impl';
+import { UserRequest } from './types';
+import userRouter from './routers/user-routes';
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig)
     await orm.getMigrator().up();
+
+    const userService = new UserServiceImpl(orm.em);
 
     const app = express();
     app.use(cors({
@@ -17,6 +22,26 @@ const main = async () => {
         credentials: true
     }))
 
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use((req, res, next) => {
+        RequestContext.create(orm.em, next);
+    });
+
+    app.use('/users', (req: UserRequest, res, next) => {
+        req.service = userService
+        next();
+    }, userRouter);
+
+    app.use((err, req, res, next) => {
+        if(err.message.includes('not found')){
+            res.status(404).send('Entity not found!');
+        }else{
+            err.status(500).send(err.message)
+        }
+    })
+    
     const redis = new Redis();
 
     const apolloServer = new ApolloServer({
@@ -24,7 +49,7 @@ const main = async () => {
             resolvers: [UserResolver],
             validate: false
         }),
-        context: ({req, res}) => ({ em: orm.em, req, res, redis }) 
+        context: ({req, res}) => ({ services: { userService }, req, res, redis }) 
     })
 
     apolloServer.applyMiddleware({ app });
