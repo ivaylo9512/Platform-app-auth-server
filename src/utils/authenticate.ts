0422238f1987930756ae097e28cffeb1124a1dac
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-13 23:40:23 +0300

    const changes

--- src/utils/authenticate.ts
+++ src/utils/authenticate.ts
@@ -1,23 +1,29 @@
 import { sign }  from 'jsonwebtoken';
-import User from 'src/entities/User';
+import User from '../entities/User';
 import { authenticate } from 'passport';
 import { CookieOptions } from 'express';
+import { DEFAULT_REFRESH_EXPIRY, DEFAULT_JWT_EXPIRY, DEFAULT_REFRESH_SECRET, DEFAULT_JWT_SECRET } from '../constants';
+
+export const refreshSecret = process.env.REFRESH_TOKEN_SECRET || DEFAULT_REFRESH_SECRET;
+export const jwtSecret = process.env.JWT_SECRET || DEFAULT_JWT_SECRET;
+const jwtExpiry = eval(process.env.JWT_EXPIRY || DEFAULT_JWT_EXPIRY);
+const refreshExpiry = eval(process.env.REFRESH_TOKEN_EXPIRY || DEFAULT_REFRESH_EXPIRY);
 
 export const COOKIE_OPTIONS: CookieOptions = {
     httpOnly: true,
     secure: true,
     signed: true,
-    maxAge: eval(process.env.REFRESH_TOKEN_EXPIRY || '60 * 60 * 24 * 50') * 1000,
+    maxAge: refreshExpiry * 1000,
     sameSite: "none",
 }
 export const getToken = (user: User) => {
-    return sign(user, process.env.JWT_SECRET || 'gsdgsdghrtSecretJwt', {
-      expiresIn: eval(process.env.SESSION_EXPIRY || '60 * 60 * 24 * 50'),
+    return sign(user, jwtSecret, {
+      expiresIn: jwtExpiry,
     })
 }
 export const getRefreshToken = (user: User) => {
-    const refreshToken = sign(user, process.env.REFRESH_TOKEN_SECRET || 'fdsgerhrehSecretJwt', {
-      expiresIn: eval(process.env.REFRESH_TOKEN_EXPIRY || '60 * 60 * 24 * 50'),
+    const refreshToken = sign(user, refreshSecret, {
+      expiresIn: refreshExpiry,
     })
     return refreshToken
 }
