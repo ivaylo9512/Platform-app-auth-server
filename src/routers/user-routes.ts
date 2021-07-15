9b705ab43462d1fdf47509f6c6abbfba789c9f76
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-15 20:27:26 +0300

    login + refreshToken + register routes changes for jwt

--- src/routers/user-routes.ts
+++ src/routers/user-routes.ts
@@ -1,7 +1,11 @@
 import { Router, Response } from "express";
 import { UserRequest } from "src/types";
-import { getToken, getRefreshToken, COOKIE_OPTIONS } from '../utils/authenticate'
-import User from "src/entities/User";
+import { getToken, getRefreshToken, COOKIE_OPTIONS, refreshExpiry } from '../authentication/authenticate'
+import User from "../entities/user";
+import { refreshSecret } from "../authentication/authenticate";
+import { JwtUser } from "../authentication/jwt-user";
+import UnauthorizedException from "src/expceptions/unauthorized";
+
 const router = Router();
 
 router.get('/findById/:id', async(req: UserRequest, res) => {
@@ -21,19 +25,55 @@ router.delete('/delete/:id', async(req: UserRequest, res) => {
 })
 
 router.post('/login', async(req: UserRequest, res: Response) => {
-    setTokens(res, req.body);
+    const userResponse = await req.service?.login(req.body);
+    const user = userResponse?.user;
 
-    res.send(await req.service?.login(req.body));
+    if(user){
+        setTokens(res, user, req);
+    }
+
+    res.send(userResponse);
 })
 router.post('/register', async(req: UserRequest, res) => {
-    setTokens(res, req.body);
+    const userResponse = await req.service?.register(req.body);
+    const user = userResponse?.user;
     
-    res.send(await req.service?.register(req.body));
+    if(user){
+        setTokens(res, user, req);
+    }
+
+    res.send(user);
 })
 
-const setTokens = (res: Response, user: User) => {
-    const token = getToken(user)
-    const refreshToken = getRefreshToken(user);
+router.get('/refreshToken', async(req: UserRequest, res) => {
+    const { signedCookies: { refreshToken } } = req
+
+    if(refreshToken){
+        try {
+            const user = await req.service?.getUserFromToken(refreshToken, refreshSecret);
+            if(!user){
+                throw new UnauthorizedException('Unauthorized.');
+            }
+        
+            const token = getToken(user)
+
+            res.header('Access-Control-Expose-Headers', 'Authorization'); 
+            res.header('Authorization', token);
+        }catch(err){
+            throw new UnauthorizedException('Incorrect refresh token.');
+        }
+    }else{
+        throw new UnauthorizedException('Refresh token is missing.');
+    }
+    
+    res.send();
+})
+
+const setTokens = (res: Response, user: User, req: UserRequest) => {
+    const token = getToken(new JwtUser(user))
+    const refreshToken = getRefreshToken(new JwtUser(user));
+
+    req.service?.addToken(user, refreshToken, refreshExpiry / 60 / 60 / 24)
 
     res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
     res.header('Access-Control-Expose-Headers', 'Authorization'); 
