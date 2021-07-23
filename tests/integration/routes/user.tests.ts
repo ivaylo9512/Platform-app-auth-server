e2fee0befa62b1df99be96f483dc0047ddd6f51e
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-23 23:58:38 +0300

    first tests user routes

--- tests/integration/routes/user.tests.ts
+++ tests/integration/routes/user.tests.ts
@@ -37,6 +37,77 @@ describe('user route tests', () => {
     beforeAll(async() => {
         ({app, orm} = await initialize());
     })
+
+    it('should create first user', async() => {
+
+        const res = await request(app)
+            .post('/users/register')
+            .set('Content-Type', 'Application/json')
+            .send(firstUser)
+            .expect(200);
+
+            firstUser.id = res.body.id;
+            firstUser.role = res.body.role;
+            firstToken = 'Bearer ' + res.get('Authorization');
+            delete firstUser.password 
+
+            expect(res.body.id).toBe(1);
+            expect(res.body).toEqual(firstUser);
+    })
+
+    it('should create users when logged user is admin', async() => {
+
+        const res = await request(app)
+            .post('/users/auth/create')
+            .set('Content-Type', 'Application/json')
+            .set('Authorization', adminToken)
+            .send([secondUser, thirdUser, forthUser])
+
+            const [{id}, {id: secondId}, {id: thirdId}] = res.body 
+            
+            secondUser.id = id;
+            thirdUser.id = secondId;
+            forthUser.id = thirdId;
+
+            delete secondUser.password;
+            delete thirdUser.password;
+            delete forthUser.password;
+
+            expect(id).toBe(2);
+            expect(secondId).toBe(3);
+            expect(thirdId).toBe(4);
+            expect(res.body).toEqual([secondUser, thirdUser, forthUser]);
+    })
+
+    it('should throw UnauthorizedException when creating user with user that is not admin', async() => {
+        const res = await request(app)
+            .post('/users/auth/create')
+            .set('Content-Type', 'Application/json')
+            .set('Authorization', firstToken)
+            .send(secondUser)
+            .expect(401);
+
+            expect(res.text).toBe('Unauthorized.');
+    })
+    
+    it('should login user with username', async() => {
+        const res = await request(app)
+            .post('/users/login')
+            .set('Content-Type', 'Application/json')
+            .send({
+                username: secondUser.username,
+                password: 'testUserPassword1'
+            })
+            .expect(200);
+
+            const refreshCookie = res.get('set-cookie').find(cookie => cookie.includes('refreshToken'));
+            expect(refreshCookie).toBeDefined();
+
+            refreshToken = refreshCookie.split(';')[0].split('refreshToken=')[1];
+            secondToken = 'Bearer ' + res.get('Authorization');
+        
+            expect(res.body).toEqual(secondUser);
+    })
     
     afterAll(async() => {
         await orm.close();
