4be814fa6c209f7ada4c46eb61a3f2b915567490
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-25 23:40:46 +0300

    user service repo +  findByUsernameOrEmail added + change logic to custom repo

--- src/services/base/user-service.ts
+++ src/services/base/user-service.ts
@@ -1,18 +1,18 @@
-import UserInput from "src/resolvers/types/login-input";
-import UserResponse from "src/resolvers/types/user-response";
 import RegisterInput from "src/resolvers/types/register-input";
 import UpdateInput from "src/resolvers/types/update-input";
 import User from "src/entities/user";
 import { JwtUser } from "src/authentication/jwt-user";
+import RefreshToken from "src/entities/refresh-token";
 
 export default interface UserService {
     findById(id: number, loggedUser: JwtUser): Promise<User>;
-    login(userInput: UserInput): Promise<UserResponse>;
-    register(registerInput: RegisterInput): Promise<UserResponse>;
+    findByUsernameOrEmail(username: string, email: string): Promise<User[]>
+    login(user: any): Promise<User>;
+    register(registerInput: RegisterInput): Promise<User>;
     delete(id: number, loggedUser: JwtUser): Promise<boolean>;
     forgotPassword(email: string): void
-    update(updateInput: UpdateInput, loggedUser: JwtUser): Promise<UserResponse>
+    update(updateInput: UpdateInput, loggedUser: JwtUser): Promise<User>
     getUserFromToken(token: string, secret: string): Promise<User>
-    addToken(user: User, token: string, expiryDays: number): void
+    addToken(user: User, refreshToken: RefreshToken, expiryDays: number): void
     removeToken(token: string, secret: string): void
 }
--- src/services/user-service-impl.ts
+++ src/services/user-service-impl.ts
@@ -2,9 +2,7 @@ import UserInput from "../resolvers/types/login-input";
 import UserResponse from "../resolvers/types/user-response";
 import User from "../entities/user";
 import argon2 from 'argon2';
-import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import UserService from "./base/user-service";
-import validateRegister from "../utils/validate-register";
 import RegisterInput from "../resolvers/types/register-input";
 import { v4 } from 'uuid';
 import { FORGOT_PASSWORD } from '../constants';
@@ -14,13 +12,14 @@ import { verify } from "jsonwebtoken";
 import RefreshToken from "../entities/refresh-token";
 import UnauthorizedException from "../expceptions/unauthorized";
 import { JwtUser } from "src/authentication/jwt-user";
+import UserRepositoryImpl from '../repositories/user-repository-impl'
 
 export default class UserServiceImpl implements UserService {
-    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+    repo: UserRepositoryImpl;
     redis: Redis;
 
-    constructor(em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>, redis: Redis){
-        this.em = em;
+    constructor(repo: UserRepositoryImpl, redis: Redis){
+        this.repo = repo;
         this.redis = redis;
     }
         
@@ -29,102 +28,62 @@ export default class UserServiceImpl implements UserService {
             throw new UnauthorizedException('Unauthorized.');
         }
         
-        return await this.em.findOneOrFail(User, { id });
+        return await this.repo.findById(id);
     }
 
-    async login(userInput: UserInput): Promise<UserResponse> {
+    async findByUsernameOrEmail(username: string, email: string){
+        return await this.repo.findByUsernameOrEmail(username, email)
+    }
+
+    async login(userInput: UserInput): Promise<User> {
         let { username, password, email } = userInput; 
 
-        const user = await this.em.findOne(User,  username ? 
+        const user = await this.repo.findUser(username ? 
             { username } : { email } 
         )
         
         if(!user || !await argon2.verify(user.password, password)) {
-            return {
-                errors: [{
-                    field: 'login',
-                    message: 'Incorrect username, email or password.'
-                }]
-            }
+            throw new UnauthorizedException('Incorrect username, pasword or email.')
         }
-        return {
-            user,
-        };
+        return user
     }
 
-    async register(registerInput: RegisterInput): Promise<UserResponse>{
-        const errors = validateRegister(registerInput);
+    async register(registerInput: RegisterInput): Promise<User>{
+        let { username, password, firstName, lastName, age, email } =  registerInput;
 
-        if(errors){
-            return{
-                errors
-            }
-        }
-
-        const password = await argon2.hash(registerInput.password);
-        const user = this.em.create(User, { 
-            username: registerInput.username, 
+        password = await argon2.hash(password);
+        const user = this.repo.create({ 
+            username, 
             password,
-            firstName: registerInput.firstName, 
-            lastName: registerInput.lastName,
-            age: registerInput.age,
-            email: registerInput.email
+            firstName, 
+            lastName,
+            age,
+            email
         })
 
-        try{
-            await this.em.persist(user).flush();
-        }catch(err){
-            if(err.code === '23505' || (err.detail && err.detail.includes('already exists'))){
-                if(err.detail.includes('email')){
-                    return{
-                        errors: [{
-                            field: 'email',
-                            message: 'Email is already in use.'
-                        }]
-                    }
-                }
+        await this.repo.flush();
 
-                return{
-                    errors: [{
-                        field: 'username',
-                        message: 'Username has already been taken'
-                    }]
-                }
-            }
-        }
-        
-        return {
-            user
-        };
+        return user;
     }
 
-    async update(updateInput: UpdateInput, loggedUser: JwtUser): Promise<UserResponse>{
-        if(updateInput.id != loggedUser.id && loggedUser.role != 'admin'){
-            throw new UnauthorizedException('Unauthorized.');
-        }
+    async update(updateInput: UpdateInput, foundUser: User): Promise<User>{
+        let { username, firstName, lastName, age, email } =  updateInput;
 
-        const user = await this.em.findOne(User, { id: updateInput.id });
-        if(!user){
-            return{
-                errors: [{
-                    field: 'id',
-                    message: `User with id: ${updateInput.id} doesn't exist.`
-                }]
-            }
-        }
+        foundUser.username = username;
+        foundUser.firstName = firstName;
+        foundUser.lastName = lastName;
+        foundUser.age = age;
+        foundUser.email = email;
 
-        UserServiceImpl.updateFields(user, updateInput);
-        await this.em.flush();
+        await this.repo.flush();
 
-        return {
-            user
-        };
+        return foundUser;
     }
     
     async getUserFromToken(token: string, secret: string){
         const payload = verify(token, secret);
         
-        const user = await this.em.findOne(User, { id: payload.id }, ['refreshTokens'])
+        const user = await this.repo.findOne({ id: payload.id }, ['refreshTokens'])
         if(!user){
             throw new UnauthorizedException('Unauthorized.');
         }
@@ -142,14 +101,14 @@ export default class UserServiceImpl implements UserService {
             throw new UnauthorizedException('Unauthorized');
         }
         
-        const user = await this.em.findOneOrFail(User, { id });
-        this.em.remove(user);
+        const user = await this.repo.findById(id);
+        this.repo.remove(user);
 
         return true;
     }
 
     async forgotPassword(email: string){
-        const user = await this.em.findOne(User, { email })
+        const user = await this.repo.findOne({ email })
         if(!user){
             return false;
         }
@@ -157,39 +116,27 @@ export default class UserServiceImpl implements UserService {
         const token = v4();
         await this.redis.set(FORGOT_PASSWORD + token, user.id, 'expire', 1000 * 60 * 60);
                 
-        await sendEmail(email, `<a href="http://localhost:3000/change-password/${token}>change password</a>`)
+        // await sendEmail(email, `<a href="http://localhost:3000/change-password/${token}>change password</a>`)
         
         return true;
     }
 
-    async addToken(user: User, token: string, expiryDays: number){
+    async addToken(user: User, refreshToken: RefreshToken, expiryDays: number){
         const date = new Date();
         date.setDate(date.getDate() + expiryDays);
 
-        const refreshToken = this.em.create(RefreshToken, { 
-            token, 
-            expiresAt: date
-        })
         user.refreshTokens.add(refreshToken);
 
-        await this.em.flush()
+        await this.repo.flush()
     }
 
     async removeToken(token: string, secret: string){
         const jwtUser = verify(token, secret);
-        const user = await this.em.findOneOrFail(User, { id: jwtUser.id }, ['refreshTokens']);
+        const user = await this.repo.findOneOrFail({ id: jwtUser.id }, ['refreshTokens']);
 
         const foundToken = user.refreshTokens.getItems().find((rt) => rt.token == token);
         if(foundToken){
-            await this.em.remove(foundToken).flush();
+            await this.repo.remove(foundToken).flush();
         }
-    }
-
-    static updateFields(user: { [name: string]: any }, fields: UpdateInput){
-        Object.entries(fields).forEach(([key, value]) => {
-            if(value != undefined){
-                user[key] = value
-            }
-        })
     }
 }
