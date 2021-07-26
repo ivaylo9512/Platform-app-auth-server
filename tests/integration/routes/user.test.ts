6bd4dc929e660b582b232fc37fec069e620daa8a
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-26 21:30:57 +0300

    user route test changes

--- tests/integration/routes/user.test.ts
+++ tests/integration/routes/user.test.ts
@@ -9,9 +9,6 @@ type User = {
     id?: number,
     username: string,
     password?: string,
-    name: string,
-    description: string,
-    location: string,
     email: string,
     role: string,
     age: number
@@ -20,9 +17,6 @@ type User = {
 const [firstUser, secondUser, thirdUser, forthUser]: User[] = Array.from({length: 4}, (_user, i) => ({
     username: 'testUser' + i, 
     password: 'testUserPassword' + i, 
-    name: 'testName' + i, 
-    description: 'test description' + i, 
-    location: 'test location' + i,
     email: `testEmail${i}@gmail.com`,
     role: 'user',
     age: 25,
@@ -33,9 +27,6 @@ const [firstUser, secondUser, thirdUser, forthUser]: User[] = Array.from({length
 export const [updatedFirstUser, updatedSecondUser]: User[] = Array.from({length: 2}, (_user, i) => ({
     id: i + 1,
     username: 'testUserUpdated' + i, 
-    name: 'testNameUpdated' + i, 
-    description: 'test description updated' + i, 
-    location: 'test location updated' + i,
     email: `testEmailUpdated${i}@gmail.com`,
     role: 'user',
     age: 25,
@@ -57,6 +48,9 @@ describe('user route tests', () => {
     beforeAll(async() => {
         ({app, orm} = await initialize());
     })
+    afterAll(async() => {
+        await orm.close();
+    })
 
     it('should create first user', async() => {
 
@@ -64,14 +58,14 @@ describe('user route tests', () => {
             .post('/users/register')
             .set('Content-Type', 'Application/json')
             .send(firstUser)
-            .expect(200);
+            .expect(200)
 
-            firstUser.id = res.body.id;
-            firstToken = 'Bearer ' + res.get('Authorization');
-            delete firstUser.password 
+        firstUser.id = res.body.id;
+        firstToken = 'Bearer ' + res.get('Authorization');
+        delete firstUser.password 
 
-            expect(res.body.id).toBe(1);
-            expect(res.body).toEqual(firstUser);
+        expect(res.body.id).toBe(1);
+        expect(res.body).toEqual(firstUser);
     })
 
     it('should retrun 422 when register user with exsisting username', async() => {
@@ -83,7 +77,7 @@ describe('user route tests', () => {
             .send(user)
             .expect(422);
 
-            expect(res.body.username).toBe('User with given username or email already exists.');
+        expect(res.body.username).toBe('User with given username or email already exists.');
     })
 
     
@@ -96,7 +90,7 @@ describe('user route tests', () => {
             .send(user)
             .expect(422);
 
-            expect(res.body.username).toBe('User with given username or email already exists.');
+        expect(res.body.username).toBe('User with given username or email already exists.');
     })
 
     it('should retrun 422 when register user with invalid fields', async() => {
@@ -104,9 +98,9 @@ describe('user route tests', () => {
             email: "Must be a valid email.", 
             password: "Password must be between 10 and 22 characters",
             username: "Username must be between 8 and 20 characters", 
-            name: "You must provide a name.", 
-            location: "You must provide a location.", 
-            description: "You must provide a description."
+            age: "You must provide age.", 
+            firstName: "You must provide a firstName.", 
+            lastName: "You must provide a lastName."
         }
 
         const res = await request(app)
@@ -115,7 +109,7 @@ describe('user route tests', () => {
             .send({})
             .expect(422);
 
-            expect(res.body).toEqual(errors);
+        expect(res.body).toEqual(errors);
     })
 
     it('should create users when logged user is admin', async() => {
@@ -127,28 +121,28 @@ describe('user route tests', () => {
         }
 
         const res = await request(app)
-            .post('/users/auth/create')
+            .post('/users/auth/createMany')
             .set('Content-Type', 'Application/json')
             .set('Authorization', adminToken)
             .send({
                 users: [secondUser, thirdUser, forthUser, adminUser]
             })
-            expect(200);
+            .expect(200);
 
-            const [{id: secondId}, {id: thirdId}, {id: forthId}, {id: fifthId, role}] = res.body 
-            
-            secondUser.id = secondId;
-            thirdUser.id = thirdId;
-            forthUser.id = forthId;
-            adminUser.id = fifthId;
+        const [{id: secondId}, {id: thirdId}, {id: forthId}, {id: fifthId}] = res.body 
+        
+        secondUser.id = secondId;
+        thirdUser.id = thirdId;
+        forthUser.id = forthId;
+        adminUser.id = fifthId;
 
-            delete secondUser.password;
-            delete thirdUser.password;
-            delete forthUser.password;
-            delete adminUser.password;
+        delete secondUser.password;
+        delete thirdUser.password;
+        delete forthUser.password;
+        delete adminUser.password;
 
-            expect([secondId, thirdId, forthId, fifthId]).toEqual([2, 3, 4, 5]);
-            expect(res.body).toEqual([secondUser, thirdUser, forthUser, adminUser]);
+        expect([secondId, thirdId, forthId, fifthId]).toEqual([2, 3, 4, 5]);
+        expect(res.body).toEqual([secondUser, thirdUser, forthUser, adminUser]);
     })
 
     it('should return 401 when creating user with user that is not admin', async() => {
@@ -160,13 +154,13 @@ describe('user route tests', () => {
         }
 
         const res = await request(app)
-            .post('/users/auth/create')
+            .post('/users/auth/createMany')
             .set('Content-Type', 'Application/json')
             .set('Authorization', firstToken)
             .send({ users: [ user ] })
             .expect(401);
 
-            expect(res.text).toBe('Unauthorized.');
+        expect(res.text).toBe('Unauthorized.');
     })
 
     it('should login user with username', async() => {
@@ -179,13 +173,13 @@ describe('user route tests', () => {
             })
             .expect(200);
 
-            const refreshCookie = res.get('set-cookie').find(cookie => cookie.includes('refreshToken'));
-            expect(refreshCookie).toBeDefined();
+        const refreshCookie = (<Array<String>>(res.get('set-cookie') as unknown)).find(cookie => cookie.includes('refreshToken'));
+        expect(refreshCookie).toBeDefined();
 
-            refreshToken = refreshCookie.split(';')[0].split('refreshToken=')[1];
-            secondToken = 'Bearer ' + res.get('Authorization');
-        
-            expect(res.body).toEqual(secondUser);
+        refreshToken = refreshCookie!.split(';')[0].split('refreshToken=')[1];
+        secondToken = 'Bearer ' + res.get('Authorization');
+    
+        expect(res.body).toEqual(secondUser);
     })
 
     it('should login user with email', async() => {
@@ -198,7 +192,7 @@ describe('user route tests', () => {
             })
             .expect(200);
 
-            expect(res.body).toEqual(thirdUser);
+        expect(res.body).toEqual(thirdUser);
     })
 
     it('should login user with email', async() => {
@@ -211,8 +205,8 @@ describe('user route tests', () => {
             })
             .expect(200);
 
-            forthToken = 'Bearer ' + res.get('Authorization');
-            expect(res.body).toEqual(forthUser);
+        forthToken = 'Bearer ' + res.get('Authorization');
+        expect(res.body).toEqual(forthUser);
     })
 
     it('should get token', async() => {
@@ -221,7 +215,7 @@ describe('user route tests', () => {
             .set('Cookie', `refreshToken=${refreshToken}`)
             expect(200);
             
-            expect(res.get('Authorization')).toBeDefined();
+        expect(res.get('Authorization')).toBeDefined();
     })
 
     it('should return 401 when login user with wrong password', async() => {
@@ -234,23 +228,25 @@ describe('user route tests', () => {
             })
             .expect(401);
 
-            expect(res.text).toBe('Incorrect username, pasword or email.');
+        expect(res.text).toBe('Incorrect username, pasword or email.');
     })
 
     it('should return firstUser when findById with id 1', async() => {
         const res = await request(app)
-            .get('/users/findById/1')
+            .get('/users/auth/findById/1')
+            .set('Authorization', firstToken)
             .expect(200);
 
-            expect(res.body).toEqual(firstUser);
+        expect(res.body).toEqual(firstUser);
     })
 
     it('should return 404 when findById with nonexistent id', async() => {
         const res = await request(app)
-            .get('/users/findById/252')
+            .get('/users/auth/findById/252')
+            .set('Authorization', adminToken)
             .expect(404);
 
-            expect(res.text).toBe('Could not find any entity of type "User" matching: {\n    "id": 252\n}');
+        expect(res.text).toBe('User not found.');
     })
 
     it('should return 401 when updating user from another loggedUser that is not admin: role', async() => {
@@ -261,7 +257,7 @@ describe('user route tests', () => {
             .send(updatedFirstUser)
             .expect(401);
 
-            expect(res.text).toBe('Unauthorized.');
+        expect(res.text).toBe('Unauthorized.');
     })
 
     it('should update user when updating with same logged user id', async() => {
@@ -272,7 +268,7 @@ describe('user route tests', () => {
             .send(updatedFirstUser)
             .expect(200);
 
-            expect(res.body).toEqual(updatedFirstUser);
+        expect(res.body).toEqual(updatedFirstUser);
     })
 
     it('should update user when updating with logged user with role: admin', async() => {
@@ -283,7 +279,7 @@ describe('user route tests', () => {
             .send(updatedSecondUser)
             .expect(200);
 
-            expect(res.body).toEqual(updatedSecondUser);
+        expect(res.body).toEqual(updatedSecondUser);
     })
 
     it('should return 401 when deleting user from another loggedUser that is not admin: role', async() => {
@@ -292,16 +288,16 @@ describe('user route tests', () => {
             .set('Authorization', secondToken)
             .expect(401);
 
-            expect(res.text).toBe('Unauthorized.');
+        expect(res.text).toBe('Unauthorized.');
     })
 
     it('should delete user when deleting with same logged user id', async() => {
         const res = await request(app)
             .delete('/users/auth/delete/4')
             .set('Authorization', forthToken)
-            expect(200)
+            expect(200);
 
-            expect(res.body).toBe(true);
+        expect(res.body).toBe(true);
     })
 
     it('should delete user when deleting with logged user with role: admin', async() => {
@@ -310,7 +306,7 @@ describe('user route tests', () => {
             .set('Authorization', adminToken)
             .expect(200);
 
-            expect(res.body).toBe(true);
+        expect(res.body).toBe(true);
     })
 
     it('should return false when deleting nonexistent user', async() => {
@@ -319,7 +315,7 @@ describe('user route tests', () => {
             .set('Authorization', forthToken)
             .expect(200);
 
-            expect(res.body).toBe(false);
+        expect(res.body).toBe(false);
     })
 
     it('should return 404 when updating nonexistent user', async() => {
@@ -330,7 +326,7 @@ describe('user route tests', () => {
             .send(forthUser)
             .expect(404);
 
-            expect(res.text).toBe(`Could not find any entity of type "User" matching: {\n    "id": ${forthUser.id}\n}`);
+        expect(res.text).toBe('User not found.');
     })
 
     it('should return 422 when creating users with wrong input', async() => {
@@ -354,7 +350,7 @@ describe('user route tests', () => {
             })
             .expect(422);
 
-            expect(res.body).toEqual(error);
+        expect(res.body).toEqual(error);
     })
 
     it('should return 422 when creating users with usernames that are already in use.', async() => {
@@ -374,6 +370,6 @@ describe('user route tests', () => {
             })
             .expect(422);
 
-            expect(res.body).toEqual(error)
+        expect(res.body).toEqual(error)
     })
 })
