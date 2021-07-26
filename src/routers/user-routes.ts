4e00d8dc8b83deb6856c2bd6e0a0d7d5b3bfdd0e
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-26 22:11:58 +0300

    validation middlewares added to routes + logged check removed + createMany route + create token changed to service

--- src/routers/user-routes.ts
+++ src/routers/user-routes.ts
@@ -1,10 +1,12 @@
-import { Router, Response, ErrorRequestHandler  } from "express";
+import { Router, Response } from "express";
 import { UserRequest } from "src/types";
 import { getToken, getRefreshToken, COOKIE_OPTIONS, refreshExpiry } from '../authentication/jwt'
 import User from "../entities/user";
 import { refreshSecret } from "../authentication/jwt";
 import { JwtUser } from "../authentication/jwt-user";
 import UnauthorizedException from "../expceptions/unauthorized";
+import UserDto from "../entities/dtos/user-dto";
+import { createValidationRules, createValidator, registerValidationRules, registerValidator, updateValidatorRules, updateValidator } from "../validators/users-validator";
 
 const router = Router();
 
@@ -14,17 +16,11 @@ router.get('/auth/findById/:id', async(req: UserRequest, res: Response) => {
         throw new UnauthorizedException('Unauthorized.');
     }
 
-    res.send(await req.service?.findById(Number(req.params.id), loggedUser));
+    res.send(new UserDto(await req.userService!.findById(Number(req.params.id), loggedUser)));
 })
 
-
-router.patch('/auth/update', async(req: UserRequest, res: Response) => {
-    const loggedUser = req.user;
-    if(!loggedUser){
-        throw new UnauthorizedException('Unauthorized.');
-    }
-
-    res.send(await req.service?.update(req.body, loggedUser));
+router.patch('/auth/update', updateValidatorRules, updateValidator, async(req: UserRequest, res: Response) => {
+    res.send(new UserDto(await req.userService!.update(req.body, req.foundUser!)));
 })
 
 router.delete('/auth/delete/:id', async(req: UserRequest, res: Response) => {
@@ -33,44 +29,48 @@ router.delete('/auth/delete/:id', async(req: UserRequest, res: Response) => {
         throw new UnauthorizedException('Unauthorized.');
     }
 
-    res.send(await req.service?.delete(Number(req.params.id), loggedUser));
+    res.send(await req.userService?.delete(Number(req.params.id), loggedUser));
 })
 
 router.post('/login', async(req: UserRequest, res) => {
-    const userResponse = await req.service?.login(req.body);
-    const user = userResponse?.user;
+    const user = await req.userService!.login(req.body);
 
     if(user){
-        setTokens(res, user, req);
+        await setTokens(res, user, req);
     }
 
-    res.send(userResponse);
+    res.send(new UserDto(user));
 })
+
 router.post('/logout', async(req: UserRequest, res) => {
     const { signedCookies: { refreshToken } } = req;
     
     if(refreshToken){
-        req.service?.removeToken(refreshToken, refreshSecret)
+        req.userService?.removeToken(refreshToken, refreshSecret)
     }
 
     res.clearCookie('refreshToken', COOKIE_OPTIONS)
     res.send();
 })
-router.post('/register', async(req: UserRequest, res) => {
-    const userResponse = await req.service?.register(req.body);
-    const user = userResponse?.user;
+
+router.post('/register', registerValidationRules, registerValidator, async(req: UserRequest, res: Response) => {
+    const user = await req.userService!.register(req.body);
     
     if(user){
-        setTokens(res, user, req);
+        await setTokens(res, user, req);
     }
 
-    res.send(userResponse);
+    res.send(new UserDto(user));
+})
+
+router.post('/auth/createMany', createValidationRules, createValidator, async(req: UserRequest, res: Response) => {
+    res.send((await req.userService!.createMany(req.body.users)).map(user => new UserDto(user)));
 })
 
 router.get('/refreshToken', async(req: UserRequest, res) => {
     const { signedCookies: { refreshToken } } = req
     
-    const user = await req.service!.getUserFromToken(refreshToken, refreshSecret);
+    const user = await req.userService!.getUserFromToken(refreshToken, refreshSecret);
 
     const token = getToken(user);
 
@@ -84,11 +84,12 @@ const setTokens = async (res: Response, user: User, req: UserRequest) => {
     const token = getToken(new JwtUser(user))
     const refreshToken = getRefreshToken(new JwtUser(user));
 
-    await req.service?.addToken(user, refreshToken, refreshExpiry / 60 / 60 / 24)
-
     res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
     res.header('Access-Control-Expose-Headers', 'Authorization'); 
     res.header('Authorization', token);
+
+    const refreshTokenEntity = req.refreshTokenService!.create(token, user) 
+    await req.userService?.addToken(user, refreshTokenEntity)
 }
 
 export default router;
