d7d0ad715e34b1b5dfc5c1de6c22f880cb6818fd
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-28 23:49:14 +0300

    refactor to new auth logic

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,103 +1,93 @@
 import { Resolver, Ctx, Arg, Mutation, Query, Int } from 'type-graphql';
 import { ApolloContext } from "src/types";
-import UserInput from './types/login-input';
+import LoginInput from './types/login-input';
 import RegisterInput from './types/register-input';
-import { jwtSecret } from '../authentication/jwt';
-import { verify } from 'jsonwebtoken';
-import UnauthorizedException from '../expceptions/unauthorized';
+import { getToken, getRefreshToken, COOKIE_OPTIONS } from '../authentication/jwt';
 import User from '../entities/user';
-import { registerValidator, updateValidator, createValidator } from 'src/validators/users-validator';
-import UserDto from 'src/entities/dtos/user-dto';
 import UpdateInput from './types/update-input';
+import { Response, Request } from 'express'
 
 @Resolver()
 export default class UserResolver{
     @Mutation(() => User)
     async forgotPassword(
         @Arg('email') email: string,
-        @Ctx() { services: { userService } }: ApolloContext 
+        @Ctx() { req }: ApolloContext 
     ){
-        userService.forgotPassword(email);
+        req.userService.forgotPassword(email);
     }
     
     @Query(() => User)
     async userById(
         @Arg('id', () => Int) id: number,
-        @Ctx() { services: { userService }, req }: ApolloContext
+        @Ctx() { req }: ApolloContext
     ): Promise<User> {
-        const loggedUser = UserResolver.getUserFromToken(req.headers?.authorization);
-        
-        return await userService.findById(id, loggedUser);
+        return await req.userService.findById(id, req.foundUser!);
     }
 
-    @Query(() => UserDto)
+    @Query(() => User)
     async userByUsername(
-        @Arg('username', () => String) username: string,
-        @Ctx() { services: { userService }, req }: ApolloContext
-    ): Promise<UserDto> {
-        const loggedUser = UserResolver.getUserFromToken(req.headers?.authorization);
-        
-        return new UserDto(await userService.findByUsername(username);
+        @Arg('username') username: string,
+        @Ctx() { req }: ApolloContext
+    ): Promise<User> {
+        return await req.userService.findByUsername(username);
     }
 
     @Mutation(() => User)
     async login(
-        @Arg('userInput') userInput: UserInput,
-        @Ctx() { services: { userService } }: ApolloContext
+        @Arg('loginInput') loginInput: LoginInput,
+        @Ctx() { req, res }: ApolloContext
     ): Promise<User> {
-        return await userService.login(userInput);
+        const user = await req.userService.login(loginInput);
+        await UserResolver.setTokens(res, user, req);
+
+        return user;
     }
 
     @Mutation(() => User)
     async register(
-        @Arg('registerInput') registerInput: RegisterInput,
-        @Ctx() { services: { userService }, req, res }: ApolloContext
+        @Arg('registerInput', { validate: false }) registerInput: RegisterInput,
+        @Ctx() { req, res }: ApolloContext
     ): Promise<User> {
-        registerValidator(req, res);
-        return await userService.register(registerInput);;
+        const user = await req.userService.register(registerInput);
+        await UserResolver.setTokens(res, user, req);
+
+        return user;
     }
 
-    @Mutation(() => UserDto[])
+    @Mutation(() => [User])
     async createMany(
-        @Arg('userInputs') userInputs: RegisterInput[],
-        @Ctx() { services: { userService }, req:, res }: ApolloContext
-    ): Promise<UserDto[]> {
-        createValidator(req, res);
-       
-        return (await userService!.createMany(userInputs)).map(user => new UserDto(user));
+        @Arg('users', () => [RegisterInput]) userInputs: RegisterInput[],
+        @Ctx() { req }: ApolloContext
+    ): Promise<User[]> {
+        return await req.userService!.createMany(userInputs);
     }
 
-    @Mutation(() => UserDto)
+    @Mutation(() => User)
     async update(
         @Arg('updateInput') updateInput: UpdateInput,
-        @Ctx() { services: { userService }, req, res }: ApolloContext
-    ): Promise<UserDto[]> {
-        updateValidator(req, res);
-       
-        return new UserDto(await userService.update(updateInput, req.foundUser))
+        @Ctx() { req }: ApolloContext
+    ): Promise<User> {
+        return await req.userService.update(updateInput, req.foundUser!)
     }
 
     @Mutation(() => Boolean)
     async delete(
         @Arg('id', () => Int) id: number,
-        @Ctx() { services: { userService }, req }: ApolloContext
+        @Ctx() { req }: ApolloContext
     ): Promise<boolean>{
-        const loggedUser = UserResolver.getUserFromToken(req.headers?.authorization);
-        
-        return await userService.delete(id, loggedUser)
+        return await req.userService.delete(id, req.foundUser!)
     }
 
-    static getUserFromToken(token?: string) {
-        if(!token){
-            throw new UnauthorizedException('Unauthorized');
-        }
-        token = token.split(' ')[1];
+    static async setTokens(res: Response, user: User, req: Request) {
+        const token = getToken(user)
+        const refreshToken = getRefreshToken(user);
     
-        const loggedUser = verify(token, jwtSecret);
-        if(!loggedUser){
-            throw new UnauthorizedException('Unauthorized')
-        }
-
-        return loggedUser;
+        res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
+        res.header('Access-Control-Expose-Headers', 'Authorization'); 
+        res.header('Authorization', token);
+    
+        const refreshTokenEntity = req.refreshTokenService!.create(token, user) 
+        await req.userService?.addToken(user, refreshTokenEntity)
     }
 }
